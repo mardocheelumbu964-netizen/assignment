22087e0319d394c5d42fd548664d66b875b64ccb
mardocheelumbu964-netizen <mardocheelumbu964@gmail.com>
2025-09-11 14:35:41 +0530

    Add files via upload

---
+++ all (1).py
@@ -0,0 +1,9 @@
+
+#task :
+a = int(input("Enter your number:"))
+b = int(input("Enter your number:"))
+
+print("addition: ",a+b )
+print("subtration: ",a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
