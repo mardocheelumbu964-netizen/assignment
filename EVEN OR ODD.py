61c0cadf95f40853e4174f9ed76d6df2bc85371a
LUMBU MAYUNDO MARDOCHEE <mardocheelumbu964@gmail.com>
2025-09-11 23:55:26 +0530

    Add files via upload

---
+++ EVEN OR ODD.py
@@ -0,0 +1,8 @@
+#program to check if a given number is even or odd
+
+N = int(input("Enter a number: "))
+
+if N % 2 == 0:
+    print("is an even number")
+else:
+    print("is an Odd number")
---
+++ iterate over 50 element and find sum using for loop.py
@@ -0,0 +1,10 @@
+# Program to calculate sum of integers from 1 to 50
+
+total = 0
+
+# Loop from 1 to 50
+for i in range(1, 51):
+    total += i
+
+# Display the final sum
+print("The sum of integers from 1 to 50 is:", total)
